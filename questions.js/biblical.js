4d216073f7ff36792dfe535edfcfbc1d5a8ac61e
STAYVERYWICKED <stayverywicked7@gmail.com>
2026-01-07 04:52:27 +0100

    Add biblicalQuestions array to questions.js

---
+++ questions.js/biblical.js
@@ -0,0 +1,4 @@
+const biblicalQuestions = [
+  {id:96, category:"biblical", difficulty:1, question:"Who built the ark?", options:["Moses","Noah","Abraham","David"], answer:"Noah"},
+  {id:97, category:"biblical", difficulty:1, question:"Who led the Israelites out of Egypt?", options:["Moses","Abraham","David","Solomon"], answer:"Moses"}
+];

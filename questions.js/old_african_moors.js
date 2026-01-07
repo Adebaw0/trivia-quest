6be49f6fb0d92b347a2584273ddfc9c3857f9b6c
STAYVERYWICKED <stayverywicked7@gmail.com>
2026-01-07 04:53:16 +0100

    Add questions about old African Moors

---
+++ questions.js/old_african_moors.js
@@ -0,0 +1,3 @@
+const oldAfricanMoorsQuestions = [
+  {id:100, category:"old african moors", difficulty:1, question:"Which city was a major center of learning in Moorish Africa?", options:["Timbuktu","Cairo","Casablanca","Marrakech"], answer:"Timbuktu"}
+];

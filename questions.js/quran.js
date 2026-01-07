ed8939af7bd12e56f617e2103d7f8dded7c7421c
STAYVERYWICKED <stayverywicked7@gmail.com>
2026-01-07 04:52:52 +0100

    Add Quran questions to questions.js

---
+++ questions.js/quran.js
@@ -0,0 +1,4 @@
+const quranQuestions = [
+  {id:98, category:"quran", difficulty:1, question:"Which prophet was swallowed by a big fish?", options:["Musa","Yunus","Ibrahim","Isa"], answer:"Yunus"},
+  {id:99, category:"quran", difficulty:1, question:"Which prophet was given the Ten Commandments?", options:["Musa","Isa","Ibrahim","Yunus"], answer:"Musa"}
+];

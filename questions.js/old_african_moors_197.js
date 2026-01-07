2db12b6ef942777f7a01ac6ffa45bd43621c0da3
STAYVERYWICKED <stayverywicked7@gmail.com>
2026-01-07 05:10:28 +0100

    Add questions for old African Moors category

---
+++ questions.js/old_african_moors_197.js
@@ -0,0 +1,3 @@
+const oldAfricanMoorsQuestions197 = [
+  {id:197, category:"old african moors", difficulty:2, question:"Which empire controlled much of North Africa before the 16th century?", options:["Malian Empire","Songhai Empire","Ghana Empire","Kanem-Bornu Empire"], answer:"Malian Empire"}
+];

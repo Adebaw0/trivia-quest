f6bb9b0d432d1328bb416b37d461a8b6debc29ef
STAYVERYWICKED <stayverywicked7@gmail.com>
2026-01-07 05:35:26 +0100

    Create video_games_351_360.js

---
+++ questions.js/video_games_351_360.js
@@ -0,0 +1,12 @@
+const videoGamesQuestions351_360 = [
+  {id:351, category:"video games", difficulty:3, question:"Which company developed The Witcher series?", options:["CD Projekt Red","Ubisoft","EA","Bethesda"], answer:"CD Projekt Red"},
+  {id:352, category:"video games", difficulty:3, question:"Which game features a hero named Geralt?", options:["The Witcher","Dark Souls","Skyrim","Assassin’s Creed"], answer:"The Witcher"},
+  {id:353, category:"video games", difficulty:3, question:"Which game is known for 'Battle Royale' mode?", options:["Fortnite","Call of Duty","PUBG","Apex Legends"], answer:"Fortnite"},
+  {id:354, category:"video games", difficulty:3, question:"Which Nintendo game features a plumber hero?", options:["Super Mario","Zelda","Metroid","Donkey Kong"], answer:"Super Mario"},
+  {id:355, category:"video games", difficulty:3, question:"Which game is famous for 'building and survival blocks'?", options:["Minecraft","Terraria","Roblox","Fortnite"], answer:"Minecraft"},
+  {id:356, category:"video games", difficulty:3, question:"Which franchise features characters like Ryu and Ken?", options:["Street Fighter","Tekken","Mortal Kombat","Soul Calibur"], answer:"Street Fighter"},
+  {id:357, category:"video games", difficulty:3, question:"Which game series includes 'Master Chief'?", options:["Halo","Gears of War","Call of Duty","Destiny"], answer:"Halo"},
+  {id:358, category:"video games", difficulty:3, question:"Which game features the island of Hyrule?", options:["The Legend of Zelda","Mario","Final Fantasy","Metroid"], answer:"The Legend of Zelda"},
+  {id:359, category:"video games", difficulty:3, question:"Which game developer made Assassin's Creed?", options:["Ubisoft","EA","Rockstar","Bethesda"], answer:"Ubisoft"},
+  {id:360, category:"video games", difficulty:3, question:"Which game franchise features Pokémons?", options:["Pokémon","Digimon","Yu-Gi-Oh","Monster Hunter"], answer:"Pokémon"}
+];

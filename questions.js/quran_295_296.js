caf3fc87556effc9668efad094432dab7f47db7c
STAYVERYWICKED <stayverywicked7@gmail.com>
2026-01-07 05:26:41 +0100

    Create quran_295_296.js

---
+++ questions.js/quran_295_296.js
@@ -0,0 +1,4 @@
+const quranQuestions295_296 = [
+  {id:295, category:"quran", difficulty:2, question:"Which prophet was swallowed by a whale?", options:["Yunus","Ibrahim","Musa","Isa"], answer:"Yunus"},
+  {id:296, category:"quran", difficulty:2, question:"Which surah is known as 'The Heart of the Qur’an'?", options:["Yasin","Al-Fatiha","Al-Baqarah","Al-Ikhlas"], answer:"Yasin"}
+];

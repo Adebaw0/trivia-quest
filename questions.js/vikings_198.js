0f2cda1bfa7d3dcb13b91dfce5909087616956ca
STAYVERYWICKED <stayverywicked7@gmail.com>
2026-01-07 05:11:12 +0100

    Add vikingsQuestions198 to questions.js

---
+++ questions.js/vikings_198.js
@@ -0,0 +1,3 @@
+const vikingsQuestions198 = [
+  {id:198, category:"vikings", difficulty:2, question:"Which region were the Vikings primarily from?", options:["Scandinavia","Britain","France","Germany"], answer:"Scandinavia"}
+];

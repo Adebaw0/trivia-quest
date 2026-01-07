4df74f9a5ee0520538ffadd59e60edc861a49e55
STAYVERYWICKED <stayverywicked7@gmail.com>
2026-01-07 05:27:50 +0100

    Create vikings_298.js

---
+++ questions.js/vikings_298.js
@@ -0,0 +1,3 @@
+const vikingsQuestions298 = [
+  {id:298, category:"vikings", difficulty:2, question:"Which famous Viking explored North America around 1000 AD?", options:["Leif Erikson","Ragnar Lothbrok","Erik the Red","Harald Hardrada"], answer:"Leif Erikson"}
+];

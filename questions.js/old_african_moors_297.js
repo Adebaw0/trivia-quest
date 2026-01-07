19bf2d5d5b1e57a3125fb4f15f604c4f95d632cb
STAYVERYWICKED <stayverywicked7@gmail.com>
2026-01-07 05:27:13 +0100

    Create old_african_moors_297.js

---
+++ questions.js/old_african_moors_297.js
@@ -0,0 +1,3 @@
+const oldAfricanMoorsQuestions297 = [
+  {id:297, category:"old african moors", difficulty:2, question:"Which dynasty ruled parts of North Africa in the 8th century?", options:["Umayyad","Abbasid","Fatimid","Ottoman"], answer:"Umayyad"}
+];
